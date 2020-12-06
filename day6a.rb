b8c59166b6ee507ddeca9c6f9e19e142f8e55e7d
João Bruno <jnnlbruno@gmail.com>
2020-12-06 15:23:02 +0000

    Fixed typo

--- day6a.rb
+++ day6a.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-p File.read(ARGV[0]).split("\n\n").map { |group| group("\n").chars.count }.sum
+p File.read(ARGV[0]).split("\n\n").map { |group| group.delete("\n").chars.uniq.count }.sum
