34da473761bf3f1a0ecffa6c10f5d0ed2c5a9f09
João Bruno <jnnlbruno@gmail.com>
2020-12-08 14:00:19 +0000

    Golfed it a little more

--- day8b.rb
+++ day8b.rb
@@ -36,14 +36,8 @@ original_program = File.readlines(ARGV[0]).map(&:strip)
 previous_instructions = []
 
 original_program.each_with_index do |line, i|
-    tokens = line.split
-
-    if tokens[0] != 'acc'
-        if tokens[0] == 'nop'
-            new_line = line.sub('nop', 'jmp')
-        else
-            new_line = line.sub('jmp', 'nop')
-        end
+    unless line.start_with? 'acc'
+        new_line = line.start_with?('nop') ? 'jmp ' : 'nop ' + line.split[1]
 
         acc = try_program(previous_instructions + [new_line] + original_program[i+1..-1])
 
