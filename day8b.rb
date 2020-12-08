861041c35d95bf46b25ade62fde0d80a480a29aa
João Bruno <jnnlbruno@gmail.com>
2020-12-08 14:04:59 +0000

    Cleared intent a bit

--- day8b.rb
+++ day8b.rb
@@ -23,26 +23,26 @@ def try_program(program)
 
         break unless program[cur]
 
-        return nil if (executed.include? cur)
+        return if (executed.include? cur)
 
         executed.push(cur)
     end
 
-    return acc
+    acc
 end
 
 original_program = File.readlines(ARGV[0]).map(&:strip)
 
 previous_instructions = []
 
-original_program.each_with_index do |line, i|
-    unless line.start_with? 'acc'
-        new_line = line.start_with?('nop') ? 'jmp ' : 'nop ' + line.split[1]
+original_program.each_with_index do |instruction, i|
+    unless instruction.start_with? 'acc'
+        new_instruction = instruction.start_with?('nop') ? 'jmp ' : 'nop ' + instruction.split[1]
 
-        acc = try_program(previous_instructions + [new_line] + original_program[i+1..-1])
+        acc = try_program(previous_instructions + [new_instruction] + original_program[i+1..-1])
 
         return p acc if acc
     end
 
-    previous_instructions.push(line)
+    previous_instructions.push(instruction)
 end
