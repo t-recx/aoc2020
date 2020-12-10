ddb7b72f92c64e572df871a2a0bb0817ae29a649
João Bruno <jnnlbruno@gmail.com>
2020-12-10 16:16:35 +0000

    Passed day10b

--- day10b.rb
+++ day10b.rb
@@ -1,59 +1,30 @@
 #!/usr/bin/env ruby
 
-# def get_possible_arrangement(previous, jolts, target)
-#     possible = []
-
-#     jolts.sort.each do |jolt|
-#         difference = jolt - previous 
-
-#         if [1, 3].include? difference
-#             previous = jolt
-#             possible.push(jolt)
-#         end
-
-#         if previous + 3 == target
-#             return possible
-#         end
-
-#         break if difference > 3
-
-#         break if previous + 3 > target
-#     end
-
-#     return nil
-# end
-
 def get_possible_arrangement_count(previous, jolts, target)
-    arrangements = []
     count = 0
 
     jolts.each_with_index do |jolt, i|
-        difference = jolt - (previous.last || 0)
+        difference = jolt - previous
 
-        if (1..3).include? difference
-            arrangements.concat get_possible_arrangement_count(previous, jolts[i+1..-1], target)
+        if difference >= 1 && difference <= 3
+            count += get_possible_arrangement_count(previous, jolts[i+1..-1], target)
+
+            previous = jolt
         end
 
-        if (1..3).include? difference
-            previous.push jolt
-
-        end
-
-        if (previous.last || 0) == target
-            arrangements.push previous
+        if previous == target
+            count += 1
             break
         end
 
         break if difference > 3
 
-        break if (previous.last || 0) > target
+        break if previous > target
     end
 
-    return arrangements
+    return count
 end
 
 jolts = File.readlines(ARGV[0]).map(&:strip).map(&:to_i).sort
 
-jolts_builtin = jolts.max
-
-p get_possible_arrangement_count([], jolts, jolts_builtin).count
+p get_possible_arrangement_count(0, jolts, jolts.max)
