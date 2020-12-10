000424e36bd061571c60a4ac108c53b00fa816dc
João Bruno <jnnlbruno@gmail.com>
2020-12-10 18:06:23 +0000

    Optimized a little bit more

--- day10b.rb
+++ day10b.rb
@@ -3,33 +3,30 @@
 def get_possible_arrangement_count(previous, jolts, target, visited = [])
     count = 0
 
-    previous_evaluation = visited.select { |x, y, _| x == previous and y == jolts }.first
+    previous_evaluation = visited.select { |x, y, _| x == previous and y == jolts.size }.first
 
-    if previous_evaluation
-        return previous_evaluation[2]
-    end
+    return previous_evaluation[2] if previous_evaluation
 
     jolts.each_with_index do |jolt, i|
         difference = jolt - previous
 
+        break if difference > 3
+
         if difference.between? 1, 3
+            if jolt == target
+                count += 1
+                break
+            end
+
             jolts_rest = jolts[i+1..-1]
-            possibilities_number = 0
 
             possibilities_number = get_possible_arrangement_count(previous, jolts_rest, target, visited)
 
-            visited.push([previous, jolts_rest, possibilities_number])  
+            visited.push [previous, jolts_rest.size, possibilities_number]
 
             count += possibilities_number
             previous = jolt
         end
-
-        if previous == target
-            count += 1
-            break
-        end
-
-        break if difference > 3
 
         break if previous > target
     end
@@ -39,4 +36,4 @@ end
 
 jolts = File.readlines(ARGV[0]).map(&:strip).map(&:to_i).sort
 
-p get_possible_arrangement_count(0, jolts, jolts.max, [])
+p get_possible_arrangement_count(0, jolts, jolts[-1], [])
