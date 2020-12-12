edd0f4c60fa1a1b8d6ccf7406a40ff82b67590f2
João Bruno <jnnlbruno@gmail.com>
2020-12-12 17:25:14 +0000

    Now using case instead of ifs

--- day12a.rb
+++ day12a.rb
@@ -7,22 +7,24 @@ x, y, angle = 0, 0, 90
 File.readlines(ARGV[0]).map(&:strip)
 .map { |x| [x[0].to_sym, x[1..-1].to_i] }
 .each do |action, value|
-    if action == :L
-        angle = (angle - value) % 360
-    elsif action == :R
-        angle = (angle + value) % 360
-    elsif action == :F
-        action = directions[angle]
+    case action
+        when :L
+            angle = (angle - value) % 360
+        when :R
+            angle = (angle + value) % 360
+        when :F
+            action = directions[angle]
     end
 
-    if action == :N
-        y -= value;
-    elsif action == :S
-        y += value;
-    elsif action == :E
-        x += value;
-    elsif action == :W
-        x -= value;
+    case action
+        when :N
+            y -= value;
+        when :S
+            y += value;
+        when :E
+            x += value;
+        when :W
+            x -= value;
     end
 end
 
--- day12b.rb
+++ day12b.rb
@@ -5,21 +5,22 @@ x, y, wx, wy = 0, 0, 10, -1
 File.readlines(ARGV[0]).map(&:strip)
 .map { |x| [x[0], x[1..-1].to_i] }
 .each do |action, value|
-    if action == 'L'
-        (value / 90).times { wx, wy = wy, -wx}
-    elsif action == 'R'
-        (value / 90).times { wx, wy = -wy, wx}
-    elsif action == 'N'
-        wy -= value;
-    elsif action == 'S'
-        wy += value;
-    elsif action == 'E'
-        wx += value;
-    elsif action == 'W'
-        wx -= value;
-    else
-        x += wx * value
-        y += wy * value
+    case action
+        when 'L'
+            (value / 90).times { wx, wy = wy, -wx}
+        when 'R'
+            (value / 90).times { wx, wy = -wy, wx}
+        when 'N'
+            wy -= value;
+        when 'S'
+            wy += value;
+        when 'E'
+            wx += value;
+        when 'W'
+            wx -= value;
+        else
+            x += wx * value
+            y += wy * value
     end
 end
 
