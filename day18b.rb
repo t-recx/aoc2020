4512c204e4a86d0b4b4e4a0d50d73acf15b1aedd
João Bruno <jnnlbruno@gmail.com>
2020-12-18 15:23:29 +0000

    Golfing it a little

--- day18b.rb
+++ day18b.rb
@@ -5,14 +5,11 @@ def solve_op(expression, op)
         opi = expression.index(op)
 
         if opi
-            first = expression[0..opi-1].split[-1].to_i
+            first = expression[0..opi-1].split[-1]
 
-            second = expression[opi+1..-1].split(' ', 2)[0].to_i
+            second = expression[opi+1..-1].split(' ', 2)[0]
 
-            value = first + second if op == '+'
-            value = first * second if op == '*'
-        
-            expression.sub!("#{first} #{op} #{second}", value.to_s)
+            expression.sub!("#{first} #{op} #{second}", [first, second].map(&:to_i).reduce(op).to_s)
         else
             return expression
         end
@@ -41,4 +38,4 @@ p File
     .readlines(ARGV[0])
     .map(&:strip)
     .map { |line| solve(line) }
-    .reduce(:+)
+    .sum
