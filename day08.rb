ac4b37316f6f3960e18e4457f649b5c31412bbd6
João Bruno <jnnlbruno@gmail.com>
2021-03-10 21:20:09 +0000

    Consolidated day8's challenges in a single file

--- day08.rb
+++ day08.rb
@@ -14,7 +14,7 @@ def execute(instruction, cur, acc)
     [cur, acc]
 end
 
-def try_program(program)
+def run_program(program)
     executed = []
     cur, acc = 0, 0
 
@@ -23,7 +23,10 @@ def try_program(program)
 
         break unless program[cur]
 
-        return if executed.include? cur
+        if executed.include? cur
+            yield(acc) if block_given?
+            return
+        end
 
         executed.push(cur)
     end
@@ -33,13 +36,15 @@ end
 
 original_program = File.readlines(ARGV[0]).map(&:strip)
 
+run_program(original_program) { |acc| p acc } 
+
 previous_instructions = []
 
 original_program.each_with_index do |instruction, i|
     unless instruction.start_with? 'acc'
         new_instruction = instruction.start_with?('nop') ? 'jmp ' : 'nop ' + instruction.split[1]
 
-        acc = try_program(previous_instructions + [new_instruction] + original_program[i+1..-1])
+        acc = run_program(previous_instructions + [new_instruction] + original_program[i+1..-1])
 
         return p acc if acc
     end
