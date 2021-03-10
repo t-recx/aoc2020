b24a687eb40c5bbe198845fbe6214f9cfd48e7ea
João Bruno <jnnlbruno@gmail.com>
2021-03-10 20:25:14 +0000

    Consolidated day3's challenges in a single file

---
+++ day03.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+lines = File.readlines(ARGV[0]).map(&:strip)
+
+w = lines.first.size
+
+p lines
+    .each_with_index
+    .count { |line, i| line[i * 3 % w] == '#' }
+
+p [[1,1],[3,1],[5,1],[7,1],[1,2]]
+    .map { |right, down|
+        lines
+            .each_with_index
+            .count { |line, i| i % down == 0 && line[(i / down) * right % w] == '#' }
+    }
+    .reduce(1, :*)
