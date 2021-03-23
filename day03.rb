1e3431396215b0aa507fe100cf069b55eccc2909
João Bruno <jnnlbruno@gmail.com>
2021-03-23 18:32:51 +0000

    Golfed it a little

--- day03.rb
+++ day03.rb
@@ -2,16 +2,15 @@
 
 lines = File.readlines(ARGV[0]).map(&:strip)
 
+indexed_lines = lines.each_with_index
+
 w = lines.first.size
 
-p lines
-    .each_with_index
-    .count { |line, i| line[i * 3 % w] == '#' }
+p indexed_lines.count { |line, i| line[i * 3 % w] == '#' }
 
 p [[1,1],[3,1],[5,1],[7,1],[1,2]]
     .map { |right, down|
-        lines
-            .each_with_index
+        indexed_lines
             .count { |line, i| i % down == 0 && line[(i / down) * right % w] == '#' }
     }
     .reduce(:*)
